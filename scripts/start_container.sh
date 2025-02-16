5503051e8277771cea3935f1bac745090540c285
LAVAN KUMAR CH <58553283+lavank179@users.noreply.github.com>
2025-02-16 22:51:14 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,6 +5,7 @@ set -e
 echo "Pulling"
 docker pull lavank179/sample-python-flask-app:latest
 
+
 # Run the Docker image as a container
 echo "Running..."
 docker run -d -p 5000:5000 lavank179/sample-python-flask-app:latest
